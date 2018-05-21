86ccf4d876853a397524fe53fb57acffde35b8d0
ValRCS <38938337+ValRCS@users.noreply.github.com>
2018-05-21 15:06:45 +0300

    Add files via upload

---
+++ cnt.py
@@ -0,0 +1,8 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers')
+parser.add_argument('integers', metavar='N', type=int, nargs='+', help='integer accumulator')
+parser.add_argument('-s','--sum', dest='accumulate', action='store_const', const=sum, default=max, help='sum the integers (default finds max)')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
